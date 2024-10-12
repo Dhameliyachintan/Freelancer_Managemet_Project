92419e9800c8e5465eb70cda2a17b72f1e290c0f
Dhameliyachintan <chintandhameliya07@gmail.com>
2024-10-12 05:58:09 +0530

    task management project

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = () => {
       <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <Link to="/" className="text-xl font-bold text-black">
-            Payment
+            Freelancer
           </Link>
           <div>
             <Link
